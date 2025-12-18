bb789dd413082386715e310853a3d47c62ae21de
nadyavozna <Nadiia_Vozna@std.suitt.edu.ua>
2025-12-18 15:53:46 +0200

    feat: basic utils with any (add, capitalize)

---
+++ src/demo.ts
@@ -0,0 +1,7 @@
+import { add, capitalize } from './index';
+
+// ПОМИЛКА ЛІНТИНГУ навмисно: подвійні лапки
+
+console.log('sum(any):', add(2, 3));
+
+console.log('capitalize(any):', capitalize('hello'));
