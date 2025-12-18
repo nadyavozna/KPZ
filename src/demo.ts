908f6a896e9d67eb6e02a8c150be2257c362ba45
nadyavozna <Nadiia_Vozna@std.suitt.edu.ua>
2025-12-18 16:29:58 +0200

    feat: add formatNumber with NumberFormatOptions

--- src/demo.ts
+++ src/demo.ts
@@ -1,4 +1,5 @@
-import { add, capitalize } from './index';
+import { add, capitalize, formatNumber } from './index';
 
 console.log('sum(typed):', add(2, 3));
 console.log('capitalize(typed):', capitalize('hello'));
+console.log('format(ok):', formatNumber(123.456, { precision: 2 }));
