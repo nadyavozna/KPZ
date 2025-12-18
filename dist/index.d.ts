d149dfe13f521aef33d36fe4b2ae9a7cfac9bedc
nadyavozna <Nadiia_Vozna@std.suitt.edu.ua>
2025-12-18 17:37:12 +0200

    chore: stabilize public API; add package exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,21 @@
-/** Мінімальні утиліти без суворої типізації */
+declare const config: {
+    APP_PRECISION: number;
+    LOG_LEVEL: "silent" | "info" | "debug";
+};
+
 declare function add(a: number, b: number): number;
 declare function capitalize(s: string): string;
+type NumberFormatOptions = {
+    precision?: number;
+    locale?: string;
+};
+declare function formatNumber(value: number, options?: NumberFormatOptions): string;
+type LogLevel = 'silent' | 'info' | 'debug';
+declare class Logger {
+    private level;
+    constructor(level: LogLevel);
+    info(msg: string): void;
+    debug(msg: string): void;
+}
 
-export { add, capitalize };
+export { type LogLevel, Logger, type NumberFormatOptions, add, capitalize, config, formatNumber };
